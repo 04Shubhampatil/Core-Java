729e325f36f2a6b7415caa492760ac35a8c61b94
04Shubhampatil <04Shubhampatil>
2025-03-24 12:35:33 +0530

    String in java
    Signed-off-by: 04Shubhampatil <04Shubhampatil>

---
+++ Strings/Strings.java
@@ -0,0 +1,28 @@
+package Strings;
+public class Strings {
+    public static void main(String[] args) {
+        String fristName = "Shubham";
+        String lastName = "Patil";
+        /*
+         & Concatination
+         * String fullName = fristName + " " + lastName;
+         * System.out.println(fullName);
+          ! String length method
+         * System.out.println(fullName.length());
+          ^CharAt
+         * for (int i = 0; i < fullName.length(); i++) {
+         * System.out.println(fullName.charAt(i));
+         * }
+         */
+        // * String Compare
+       /* 
+        if (fristName.compareTo(lastName) == 0) {
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strin are not equal");
+
+        }
+            */
+
+    }
+}
