08a095272789d3fe7df6f163051f7b62bd264d8d
04Shubhampatil <04Shubhampatil>
2025-03-24 13:15:27 +0530

    string methods
    Signed-off-by: 04Shubhampatil <04Shubhampatil>

---
+++ Strings/StringsMethod.java
@@ -0,0 +1,15 @@
+
+
+import java.util.Scanner;
+
+public class StringsMethod {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter your name");
+        String name = sc.nextLine();
+        System.out.println("Name is "+name);
+        sc.close();
+        
+
+    }
+}
